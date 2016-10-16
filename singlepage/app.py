3d3f66492509fe6ec75f443f22e21264ab8c23b2
dustyshap <dustin.shapiro@braintreepayments.com>
2016-10-16 14:52:14 -0500

    import v1

---
+++ singlepage/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from models import *
+
+
+app = Flask(__name__)
+initialize_db()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+
+@app.route('/results', methods=['POST'])
+def results():
+
+    search_term = request.form['tags'].lower().strip()
+    drops = Drops.select().where(Drops.tags.contains(search_term))
+
+    if not drops:
+        return render_template('/results.html', no_drops = True,
+                           search_term=search_term)
+
+    return render_template('/results.html', drops = drops,
+                           search_term=search_term)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ singlepage/models.py
@@ -0,0 +1,22 @@
+from peewee import *
+
+db = SqliteDatabase('staticmeta/drops.db')
+
+class Drops(Model):
+
+	id = PrimaryKeyField()
+	filename = TextField()
+	speaker = TextField()
+	tags = TextField()
+	transcription = TextField()
+
+	class Meta:
+		database = db
+
+
+def initialize_db():
+	db.connect()
+	db.create_tables([Drops], safe=True)
+
+
+
