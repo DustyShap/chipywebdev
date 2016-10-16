95958283b9e04093ff97f3aa66abebb8b6fcced2
dustyshap <dustin.shapiro@braintreepayments.com>
2016-10-16 14:44:30 -0500

    single page clip

--- ajax/process.py
+++ ajax/process.py
@@ -2,22 +2,24 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('form.html')
+    return render_template('form.html')
+
 
 @app.route('/process', methods=['POST'])
 def process():
+    email = request.form['email']
+    name = request.form['name']
 
-	email = request.form['email']
-	name = request.form['name']
+    if name and email:
+        newName = name[::-1]
 
-	if name and email:
-		newName = name[::-1]
+        return jsonify({'name': newName})
 
-		return jsonify({'name' : newName})
+    return jsonify({'error': 'Missing data!'})
 
-	return jsonify({'error' : 'Missing data!'})
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
