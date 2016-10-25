6a48fa6a59cb613359c099c041a767be689674a2
dustyshap <dustin.shapiro@braintreepayments.com>
2016-10-24 20:15:10 -0500

    Generating dynamic result objects

--- layoutv2/static/js/scripts.js
+++ layoutv2/static/js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     var $results = $("#results_container")
     var url = "<source src=http://insidestlaudio.com/drops/drops%201/"
+    var $result_object = $("#result_object")
 
     $('form').on('submit', function(event) {
 
@@ -21,15 +22,26 @@ $(document).ready(function(){
             if (data.filename.length < 1){
 
                 $results.hide();
-                $('#errorAlert').text('No Results!').show();
+                $('#errorAlert').text('No Results!').css('display','flex');
             }
 
             for (var i=0; i < data.filename.length; i++){
 
                 $('#errorAlert').hide();
                 $results.show();
-               var filename = data.filename[i].filename
-               $(".result_object").clone().appendTo($results)
+               var filename = data.filename[i].filename;
+               var speaker = data.filename[i].speaker;
+               var transcription = data.filename[i].transcription;
+               var $resulttest = $("#result"+i);
+               $result_object.clone().appendTo($("#results_container")).attr('id', 'result'+i).addClass("search_result");
+               $("#result"+i + " #speaker").text("Speaker: " + speaker);
+               $("#result"+i + " #transcription").text("Transcription: " + transcription);
+
+
+
+
+
+
             }
         });
 
