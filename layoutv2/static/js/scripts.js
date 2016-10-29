0174757d35a7b7b56025a42b4bd3176d7db7e4a0
dustyshap <dustin.shapiro@braintreepayments.com>
2016-10-29 15:38:03 -0500

    Dropping things

--- layoutv2/static/js/scripts.js
+++ layoutv2/static/js/scripts.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 
-
-
-
     var $results = $("#results_container")
     var url = "<source src=http://insidestlaudio.com/drops/drops%201/"
     var $result_object = $("#result_object")
@@ -61,7 +58,56 @@ $(document).ready(function(){
 
 });
 
+function dropped(e){
 
+    var target = e.target;
+    target.removeChild(target.firstChild);
+    var data = e.dataTransfer.getData('Text');
+    var x = document.createElement("AUDIO");
+    x.setAttribute("src", data);
+    x.setAttribute('class', 'audio_drop');
+    x.controls=true
+    target.appendChild(x);
+
+
+}
+
+
+function dragStart(e){
+    var target = e.target;
+    var audio_container = target.firstElementChild;
+    var audio = audio_container.firstElementChild;
+    var audio_source = audio.firstElementChild;
+    var source = audio_source.getAttribute('src');
+    e.dataTransfer.setData('Text', source);
+
+}
+
+
+
+function clickme(e){
+
+    var results = document.getElementsByClassName('search_result');
+}
+
+
+function doFirst(){
+
+    var button = document.getElementById('bttn');
+    var theGrid = document.getElementById('main_wrap');
+    var theParent = document.getElementById("results_container");
+    button.addEventListener("click", clickme, false);
+    theParent.addEventListener("dragstart", dragStart, false);
+    theGrid.addEventListener("dragenter", function(e){e.preventDefault();}, false);
+    theGrid.addEventListener("dragover", function(e){e.preventDefault();}, false);
+    theGrid.addEventListener("drop", dropped, false);
+
+
+
+}
+
+
+window.addEventListener("load", doFirst, false);
 
 
 
